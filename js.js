d0bf2ac3d826b599f69408f0e4ecd0dc4b038c26
NILT <100668725+NILT-Of@users.noreply.github.com>
2024-01-28 20:00:29 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+// js.js
+// You can add your JavaScript code here
+
+function handleButtonClick() {
+    // Redirect to the GitHub link when the button is clicked
+    window.location.href = 'https://github.com/NILT-Of';
+  }
+  
+  function toggleMobileMenu() {
+    // Toggle the visibility of the mobile menu (for demonstration purposes)
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+  }
+  
